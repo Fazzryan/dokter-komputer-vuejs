fe3968d7e1b387fb8f5bb43cd1ada183894060a3
Dinda Fazryan <71650858+Fazzryan@users.noreply.github.com>
2022-06-27 23:33:14 +0700

    Update ProdukView.vue

--- src/views/ProdukView.vue
+++ src/views/ProdukView.vue
@@ -184,34 +184,34 @@ export default {
     allProduk() {
       this.kategori = "Semua Produk";
       axios
-        .get("http://localhost:3000/produk")
+        .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk")
         .then((response) => this.setProduct(response.data))
         .catch((error) => console.log("ERROR", error));
     },
     cariKomputer(komputer) {
       this.kategori = komputer;
       axios
-        .get("http://localhost:3000/produk?kategori=" + komputer)
+        .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk?kategori=" + komputer)
         .then((response) => this.setProduct(response.data))
         .catch((error) => console.log("ERROR", error));
     },
     cariKomponenPc(komponenPc) {
       this.kategori = komponenPc;
       axios
-        .get("http://localhost:3000/produk?kategori=" + komponenPc)
+        .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk?kategori=" + komponenPc)
         .then((response) => this.setProduct(response.data))
         .catch((error) => console.log("ERROR", error));
     },
     cariAksesoris(aksesoris) {
       this.kategori = aksesoris;
       axios
-        .get("http://localhost:3000/produk?kategori=" + aksesoris)
+        .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk?kategori=" + aksesoris)
         .then((response) => this.setProduct(response.data))
         .catch((error) => console.log("ERROR", error));
     },
     searchProduct() {
       axios
-        .get("http://localhost:3000/produk?q=" + this.search)
+        .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk?q=" + this.search)
         .then((response) => this.setProduct(response.data))
         .catch((error) => console.log("ERROR", error));
     },
@@ -222,7 +222,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://localhost:3000/produk")
+      .get("https://my-json-server.typicode.com/Fazzryan/dokter-komputer-api/produk")
       .then((response) => this.setProduct(response.data))
       .catch((error) => console.log("ERROR", error));
   },
